d1acc8c46170096318ffd92e9b8582600ec35c02
Лапихин Александр <94218678+Al-Lapik@users.noreply.github.com>
2022-02-14 18:59:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,17 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    if (a < b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    int r = a % b;
+    if (r == 0) return b;
+    else {
+        a = b;
+        b = r;
+        return gcd(a, b);
+    }
 }
